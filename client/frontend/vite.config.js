75468c8c1b41fa4613852096a97f8ee887492150
deepak-chaudhary-211 <resodeepakchaudhary@gmail.com>
2025-08-02 02:26:21 +0530

    Update vite.config.js

--- client/frontend/vite.config.js
+++ client/frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/run': 'http://localhost:5000',
+      '/run': 'https://compilex-ft0f.onrender.com',
     },
   },
 });
